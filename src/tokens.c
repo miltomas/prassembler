01951208071c2048510639ca89273d06e41d365e
tomaspc <mil-tomas@seznam.cz>
2025-05-25 19:25:33 +0200

    chore: sigma commit

--- src/tokens.c
+++ src/tokens.c
@@ -4,9 +4,15 @@
 #include "memaccess.h"
 #include "prefixes.h"
 #include "registers.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define TKN_LINE_MAX 16
+
+// if set, output errors, exit
+int g_tkn_error;
 
 int tkn_parse_line(FILE *file, struct Token **buf) {
 	static u_long line;
@@ -14,9 +20,8 @@ int tkn_parse_line(FILE *file, struct Token **buf) {
 	char *cbuf = malloc(n);
 
 	long len = getline(&cbuf, &n, file);
-	if (len == -1) {
-		return -1;		
-	}
+	if (len == -1)
+		return -1;
 	if ((u_long)len != strlen(cbuf)) {
 		fprintf(stderr, ERR LICO "Null byte detected!\n", line, len);
 		return 1;
@@ -29,18 +34,35 @@ int tkn_parse_line(FILE *file, struct Token **buf) {
 
 	for (int i = 0; word != NULL; i++) {
 
-		if (instr_try_parse(word, &buf[i]->instr)) {
-			buf[i]->type = INSTRUCTION;
-		} else if (reg_try_parse(word, &buf[i]->reg)) {
-			buf[i]->type = REGISTER;
-		} else if (mem_try_parse(word, &buf[i]->mem)) {
+		if (i == TKN_LINE_MAX)
+			break;
+		if (word[0] == ';')
+			break;
+
+		if (word[0] == '[') {
 			buf[i]->type = MEMACCESS;
-		} else if (imm_try_parse(word, &buf[i]->imm)) {
+			if (!mem_try_parse(word, &buf[i]->mem)) {
+				fprintf(stdout, LICO ERR "Malformed memaccess!", line, word - cbuf);
+				g_tkn_error = 1;
+			}
+			continue;
+		}
+		if (isdigit(word[0])) {
 			buf[i]->type = IMMEDIATE;
+			if (!imm_try_parse(word, &buf[i]->imm)) {
+				fprintf(stdout, LICO ERR "Malformed immediate!", line, word - cbuf);
+				g_tkn_error = 1;
+			}
+			continue;
+		}
+
+		if (reg_try_parse(word, &buf[i]->reg)) {
+			buf[i]->type = REGISTER;
 		} else if (prefix_try_parse(word, &buf[i]->prefix)) {
 			buf[i]->type = PREFIX;
+		} else if (instr_try_parse(word, &buf[i]->instr)) {
+			buf[i]->type = INSTRUCTION;
 		} else {
-			// handle comments too
 			buf[i]->type = LABEL;
 		}
 
