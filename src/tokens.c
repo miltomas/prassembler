474434dcae2e5da7f10a114b2a761d4ccaeb87ca
tomaspc <mil-tomas@seznam.cz>
2025-05-27 20:33:05 +0200

    refactor: add label parsing template

--- src/tokens.c
+++ src/tokens.c
@@ -14,8 +14,10 @@
 // if set, output errors, exit
 int g_tkn_error;
 
-int tkn_parse_line(FILE *file, struct Token **buf) {
+int tkn_parse_line(FILE *file, struct Token **tkn_buf
+				   /* label declarations */) {
 	static u_long line;
+
 	size_t n = 128;
 	char *cbuf = malloc(n);
 
@@ -32,10 +34,30 @@ int tkn_parse_line(FILE *file, struct Token **buf) {
 	char *word, *saveptr;
 	word = strtok_r(cbuf, " \t", &saveptr);
 
-	for (int i = 0; word != NULL; i++) {
+	int i = 0;
+	while (word != NULL) {
 
 		if (word[0] == ';')
 			break;
+
+		char *const colon = strchr(word, ':');
+
+		if (colon) {
+			if (colon == word) {
+				PERRLICO("Malformed label!", line, colon - cbuf);
+				g_tkn_error = 1;
+			}
+			char *const next_char = colon + 1;
+			const int overflow = next_char - cbuf == len;
+
+			word = overflow || *next_char == '\0'
+					   ? strtok_r(NULL, " \t", &saveptr)
+					   : next_char;
+			// save label to caller buffer
+			// ...
+			continue;
+		}
+
 		if (i == TKN_LINE_MAX) {
 			PERRLICO("Too many tokens!", line, (u_long)0);
 			g_tkn_error = 1;
@@ -43,29 +65,28 @@ int tkn_parse_line(FILE *file, struct Token **buf) {
 		}
 
 		if (word[0] == '[') {
-			buf[i]->type = MEMACCESS;
-			if (!mem_try_parse(word, &buf[i]->mem)) {
+			tkn_buf[i]->type = MEMACCESS;
+			if (!mem_try_parse(word, &tkn_buf[i]->mem)) {
 				PERRLICO("Malformed memaccess!", line, word - cbuf);
 				g_tkn_error = 1;
 			}
-		}
-		else if (isdigit(word[0])) {
-			buf[i]->type = IMMEDIATE;
-			if (!imm_try_parse(word, &buf[i]->imm)) {
+		} else if (isdigit(word[0])) {
+			tkn_buf[i]->type = IMMEDIATE;
+			if (!imm_try_parse(word, &tkn_buf[i]->imm)) {
 				PERRLICO("Malformed immediate!", line, word - cbuf);
 				g_tkn_error = 1;
 			}
-		}
-		else if (reg_try_parse(word, &buf[i]->reg)) {
-			buf[i]->type = REGISTER;
-		} else if (prefix_try_parse(word, &buf[i]->prefix)) {
-			buf[i]->type = PREFIX;
-		} else if (instr_try_parse(word, &buf[i]->instr)) {
-			buf[i]->type = INSTRUCTION;
+		} else if (reg_try_parse(word, &tkn_buf[i]->reg)) {
+			tkn_buf[i]->type = REGISTER;
+		} else if (prefix_try_parse(word, &tkn_buf[i]->prefix)) {
+			tkn_buf[i]->type = PREFIX;
+		} else if (instr_try_parse(word, &tkn_buf[i]->instr)) {
+			tkn_buf[i]->type = INSTRUCTION;
 		} else {
-			buf[i]->type = LABEL;
+			tkn_buf[i]->type = LABEL;
 		}
 
+		i++;
 		word = strtok_r(NULL, " \t", &saveptr);
 	}
 
