658fba9580bd44f056e71e879edee336a943a51a
tomasntb <mil-tomas@seznam.cz>
2025-06-04 21:28:07 +0200

    feat: add macro to output line with diagnostic alongside message

--- headers/tokens.h
+++ headers/tokens.h
@@ -14,8 +14,15 @@ extern u_long g_tkn_line_num;
 // saved last parsed line, not freed!
 extern char *g_tkn_last_line;
 
+#define PDIAGLINE(diagnostic, message, column, ...)                            \
+	fprintf(stderr,                                                            \
+			diagnostic LICO message "\n%s\n"                                   \
+									"%*s^\n\n",                                \
+			g_tkn_line_num, column, ##__VA_ARGS__, g_tkn_last_line,            \
+			(int)(column), "")
+
 // free last line before calling again!
 extern int tkn_parse_line(FILE *file, struct Token *(*tkn_buf)[TKN_LINE_MAX],
-				   int *label_n, struct Label **label_buf);
+						  int *label_n, struct Label **label_buf);
 
 #endif
