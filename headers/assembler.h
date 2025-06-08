8c90bcfce228eb1b77702a9f997fe776492a07d4
tomaspc <mil-tomas@seznam.cz>
2025-06-08 14:41:53 +0200

    feat: add FSA to parse memaccess

--- headers/assembler.h
+++ headers/assembler.h
@@ -12,30 +12,39 @@
 typedef enum { RAW, ELF } EOutFormat;
 
 typedef enum {
-    A, C, D, B,
-    AH_SP, CH_BP, DH_SI, BH_DI,
-    R8, R9, R10, R11, R12, R13, R14, R15
+	A,
+	C,
+	D,
+	B,
+	AH_SP,
+	CH_BP,
+	DH_SI,
+	BH_DI,
+	R8,
+	R9,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	R15
 } EGPRegister;
 
-typedef enum {
-    BYTE = 1,
-    WORD = 2,
-    DWORD = 4,
-    QWORD = 8
-} ESize;
+typedef enum { BYTE = 1, WORD = 2, DWORD = 4, QWORD = 8 } ESize;
 
+#define ETOKEN_TYPE_COUNT 6
 typedef enum {
-    INSTRUCTION,
-    IMMEDIATE,
-    MEMACCESS,
-    REGISTER,
-    PREFIX,
-    LABEL
+	INSTRUCTION,
+	IMMEDIATE,
+	MEMACCESS,
+	REGISTER,
+	PREFIX,
+	LABEL
 } ETokenType;
 
 struct InstrFuncs;
 struct Instruction {
-    struct InstrFuncs *funcs;
+	struct InstrFuncs *funcs;
 };
 
 struct Immediate {
@@ -45,48 +54,46 @@ struct Immediate {
 		int16_t value16;
 		int8_t value8;
 	};
-    ESize size;
+	ESize size;
 };
 
 typedef struct {
-    ESize target_size;
-    ESize scale;
-    EGPRegister index;
-    EGPRegister base;
-    int32_t displacement;
+	ESize target_size;
+	ESize scale;
+	EGPRegister index;
+	EGPRegister base;
+	int32_t displacement;
 } MemAccess;
 
 struct Register {
-    ESize size;
-    EGPRegister type;
+	ESize size;
+	EGPRegister type;
 };
 
-struct Prefix {
-    
-};
+struct Prefix {};
 
 struct Label {
-    char *value;
+	char *value;
 };
 
 struct Token {
-    union {
-        struct Instruction *instr;
-        struct Immediate imm;
-        MemAccess *mem;
-        struct Register reg;
-        struct Prefix prefix;
-        struct Label label;
-    };
-    int8_t type;
+	int column;
+	union {
+		struct Instruction *instr;
+		struct Immediate imm;
+		MemAccess *mem;
+		struct Register reg;
+		struct Prefix prefix;
+		struct Label label;
+	};
+	int8_t type;
 };
 
-struct Operand {
-};
+struct Operand {};
 
 struct InstrFuncs {
-    int (*validate)(int cnt, struct Token tokens[cnt]);
-    int16_t (*encode)(struct Instruction *instr);
+	int (*validate)(int cnt, struct Token tokens[cnt]);
+	int16_t (*encode)(struct Instruction *instr);
 };
 
 #endif
--- src/memaccess.c
+++ src/memaccess.c
@@ -1,5 +1,100 @@
 #include "assembler.h"
+#include "tokens.h"
+#include <sys/types.h>
 
-int mem_try_parse(char *str, MemAccess **target) {
-	return 1;
+// final value must not be 0 and every field may be included once
+enum mem_EState {
+	MEM_NONE = 0,
+	MEM_SI = 1 << 0,
+	MEM_REG = 1 << 1,
+	MEM_IMM = 1 << 2
+};
+
+static int mem_transition_si(struct Token *token1, struct Token *token2,
+							 enum mem_EState _) {
+	if (token2->type == IMMEDIATE) {
+		struct Token *temp = token2;
+		token2 = token1;
+		token1 = temp;
+	}
+
+	int valid = token1->imm.size == BYTE;
+	for (int i = 0; i < 4; i++) {
+		if ((1 << i) == token1->imm.value8) {
+			valid &= 1;
+		}
+	}
+
+	return valid;
+}
+
+struct mem_StateNode;
+struct mem_StateNodeTransition {
+	// generic function to check tokens
+	int (*transition_check)(struct Token *node1, struct Token *node2,
+						 enum mem_EState transition);
+	struct mem_StateNode *node[ETOKEN_TYPE_COUNT];
+	enum mem_EState transition_state;
+};
+
+struct mem_StateNode {
+	// index with operators
+	struct mem_StateNodeTransition *transitions[2];
+	// state given if not overriden by transition state
+	enum mem_EState state;
+};
+
+//
+// states and transitions
+//
+
+static struct mem_StateNode mem_node_immediate;
+static struct mem_StateNode mem_node_register;
+
+static struct mem_StateNodeTransition mem_transition_si_immediate = {
+	.transition_check = mem_transition_si,
+	.node = {[REGISTER] = &mem_node_register},
+	.transition_state = MEM_SI};
+
+static struct mem_StateNodeTransition mem_transition_si_register = {
+	.transition_check = mem_transition_si,
+	.node = {[IMMEDIATE] = &mem_node_immediate},
+	.transition_state = MEM_SI};
+
+static struct mem_StateNodeTransition mem_transition_default = {
+	.transition_check = NULL,
+	.node =
+	{
+		[REGISTER] = &mem_node_register,
+		[IMMEDIATE] = &mem_node_immediate,
+	},
+	.transition_state = MEM_NONE};
+
+static struct mem_StateNode mem_node_immediate = {
+	.transitions = {&mem_transition_si_register, &mem_transition_default},
+	.state = MEM_IMM};
+
+static struct mem_StateNode mem_node_register = {
+	.transitions = {&mem_transition_si_immediate, &mem_transition_default},
+	.state = MEM_REG};
+
+//
+// states and transitions
+//
+
+int mem_try_parse(char *str, MemAccess **target, char *saveptr) {
+	int i = 0;
+	int c = 0;
+
+	enum mem_EState state_node;
+	if (str[0] != '[') {
+		return 0;
+	}
+
+	while ((c = str[++i]) != '\0') {
+		/*
+	 * if label => mem_ParsingState.displacement = 1;
+	 * if register =>
+	 */
+	}
 }
