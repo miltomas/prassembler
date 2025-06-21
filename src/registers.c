60b980f6ca214e779f010c79583026589cd45848
tomaspc <mil-tomas@seznam.cz>
2025-06-21 21:53:24 +0200

    chore: use gperf to parse registers

--- src/registers.c
+++ src/registers.c
@@ -1,5 +1,19 @@
 #include "assembler.h"
+#include <stdio.h>
+#include <string.h>
+
+struct GperfReg {
+	const char *name;
+	const struct Register reg;
+};
+
+const char *lookup_registers(register const char *str, register size_t len);
 
 int reg_try_parse(const char *str, struct Register *target) {
+	const struct GperfReg *gperf =
+		(struct GperfReg *)lookup_registers(str, strlen(str));
+	if (!gperf)
+		return 0;
+	*target = gperf->reg;
 	return 1;
 }
