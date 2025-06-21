7ce0ba0b5d7be27611d32fccf15ec1a8b31b57d8
tomasntb <mil-tomas@seznam.cz>
2025-06-21 10:30:50 +0200

    chore: add instruction try parse body

--- src/instructions.c
+++ src/instructions.c
@@ -10,5 +10,10 @@ struct GperfInstr {
 const char *lookup_instructions(register const char *str, register size_t len);
 
 int instr_try_parse(const char *str, struct Instruction *target) {
+	const struct GperfInstr *gperf =
+		(struct GperfInstr *)lookup_instructions(str, strlen(str));
+	if (!gperf)
+		return 0;
+	*target = gperf->instr;
 	return 1;
 }
